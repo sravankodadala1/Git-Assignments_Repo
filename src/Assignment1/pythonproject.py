8f4c115bd57629adc232664a7bb13f874374f3d6
sravan github <sravankumar04032001@gmail.com>
2023-11-14 18:11:16 +0530

    created a new print statement

--- src/Assignment1/pythonproject.py
+++ src/Assignment1/pythonproject.py
@@ -1,5 +1,7 @@
 name = input("Enter your name: ")
 age = input("Enter your age: ")
 
+
 # Print a personalized greeting
+print(f"Hello, {name}! You are {age} years old.")
 print(f"Hello, {name}! You are {age} years old.")
