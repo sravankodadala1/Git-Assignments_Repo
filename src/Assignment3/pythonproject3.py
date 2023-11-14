747108acefceff2e30bc0ba0973962517f28572e
sravan github <sravankumar04032001@gmail.com>
2023-11-15 00:06:14 +0530

    modified the code

--- src/Assignment3/pythonproject3.py
+++ src/Assignment3/pythonproject3.py
@@ -8,4 +8,4 @@ width = float(input("Enter the width of the rectangle: "))
 
 # Calculate and print the area
 area = calculate_rectangle_area(length, width)
-print(f"The area of the rectangle is: {area}")git add
+print(f"The area of the rectangle")
