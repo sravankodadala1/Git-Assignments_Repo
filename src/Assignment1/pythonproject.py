bd0c0d371e38571788dd70f51b44c71981e4b918
sravan github <sravankumar04032001@gmail.com>
2023-11-14 18:13:37 +0530

    updated gender statement

--- src/Assignment1/pythonproject.py
+++ src/Assignment1/pythonproject.py
@@ -1,7 +1,9 @@
 name = input("Enter your name: ")
 age = input("Enter your age: ")
+gender = input("Enter your gender")
 
 
 # Print a personalized greeting
 print(f"Hello, {name}! You are {age} years old.")
 print(f"Hello, {name}! You are {age} years old.")
+print(f"Hello, {name}! You are {gender}")
