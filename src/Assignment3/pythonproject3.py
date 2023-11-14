fb2cda906d61ce477806d26bca50f91d80cb8918
sravan github <sravankumar04032001@gmail.com>
2023-11-14 19:24:26 +0530

    created a python code to calculate area

---
+++ src/Assignment3/pythonproject3.py
@@ -0,0 +1,11 @@
+def calculate_rectangle_area(length, width):
+    area = length * width
+    return area
+
+# Get user input for length and width
+length = float(input("Enter the length of the rectangle: "))
+width = float(input("Enter the width of the rectangle: "))
+
+# Calculate and print the area
+area = calculate_rectangle_area(length, width)
+print(f"The area of the rectangle is: {area}")git add
