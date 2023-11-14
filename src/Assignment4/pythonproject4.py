525c4cc2ceda6ddd6a8279c8b4e9e8b2907afeeb
sravan github <sravankumar04032001@gmail.com>
2023-11-14 23:58:15 +0530

    created a project

---
+++ src/Assignment4/pythonproject4.py
@@ -0,0 +1,9 @@
+def factorial(n):
+    if n == 0 or n == 1:
+        return 1
+    else:
+        return n * factorial(n - 1)
+
+# Example: Calculate the factorial of 5
+result = factorial(5)
+print(f"The factorial of 5 is: {result}")
