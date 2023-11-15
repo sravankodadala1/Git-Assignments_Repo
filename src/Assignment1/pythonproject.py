41c71e0df591052563c1f3fa786f8547219afd4d
sravankodadala1 <150257689+sravankodadala1@users.noreply.github.com>
2023-11-15 18:32:03 +0530

    pulled and merged changes from clone repository

--- src/Assignment1/pythonproject.py
+++ src/Assignment1/pythonproject.py
@@ -6,5 +6,5 @@ gender = input("Enter your gender")
 # Print a personalized greeting
 print(f"Hello, {name}! You are {age} years old.")
 print(f"Hello, {name}! You are {age} years old.")
-print(f"Hello, {name}! You are {gender}")
+print(f"Hello, {name}! You are {gender}") 
 
