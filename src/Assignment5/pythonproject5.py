b47706c54360c18cb5810e971b12420156fcfa1f
sravan github <sravankumar04032001@gmail.com>
2023-11-15 11:19:42 +0530

    modified and commited

--- src/Assignment5/pythonproject5.py
+++ src/Assignment5/pythonproject5.py
@@ -3,3 +3,5 @@ age = input("Enter your age: ")
 
 # Display a personalized message
 print("Hello, {}! You are {} years old.".format(name, age))
+print(f"You will turn 100 years old in the year {2023 + 100 - age}.")
+print("Thank you for using this program!")
